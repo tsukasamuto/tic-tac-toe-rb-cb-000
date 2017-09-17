c4f52876b96c36a0a9702c07a45e8717e287e3f5
Tsukasa Muto <tsukasa610@gmail.com>
2017-09-17 00:23:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,14 +10,46 @@ WIN_COMBINATIONS = [
   [2, 4, 6], # Diagonal 2
 ]
 
+
 def display_board(board)
-  
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
 
 
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
+end
+
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
 
 
@@ -45,23 +77,26 @@ def won?(board)
   end
 end
 
+
 def full?(board)
   board.all? do |token|
   token == "X" || token == "O"
   end
 end
 
+
 def draw?(board)
   full?(board) && !won?(board)
 end
 
+
 def over?(board)
   won?(board) || draw?(board) || draw?(board)
 end
+
 
 def winner(board)
   if winner = won?(board)
     board[winner.first]
   end
 end
-
